35633081aeb93a1ba8657a37bb25b2a9de125663
Francis Bissonnette <francisbissonnette@gmail.com>
2015-01-05 15:15:58 -0500

    User system

--- kompanio.js
+++ kompanio.js
@@ -1,6 +1,51 @@
+
+
+
+
 if (Meteor.isClient) {
 
-}
+
+
+  Template.app.helpers({
+
+    username: function(){
+
+
+
+
+    },
+
+
+  });
+
+
+
+
+  Template.app.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+
+
+      var userName = template.find('#userName').value;
+      Meteor.users.update({_id: Meteor.user()._id}, {$set: {"profile.name":userName}});
+
+      var user = Meteor.users.find({_id: Meteor.user()._id});
+      //console.log(user.profile);
+
+
+
+    }//'submit form'
+
+
+  })
+
+
+
+
+
+
+
+}// /isClient
 
 
 
